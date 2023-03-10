840eae086f791b203631e999ea21c1de70712eb7
enclar <107608010+enclar@users.noreply.github.com>
2023-03-10 15:41:00 +0800

    feat: changed task_app views to utilize generic class-based views

--- backend/task_app/api/views.py
+++ backend/task_app/api/views.py
@@ -9,29 +9,41 @@ from task_app.models import Task
 from task_app.api.serializers import TaskSerializer
 
 # Create your views here.
-class TaskList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#! Creating views using generic class-based views
+class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-class TaskDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+#! Creating views using mixins
+# class TaskList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
     
+# class TaskDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+   
+#! Creating views using class-based views 
 # class TaskAV(APIView):
 #     def get(self, request):
 #         tasks = Task.objects.all()
