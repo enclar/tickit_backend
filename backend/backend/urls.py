b84f41ca6b7562a76aa6166cbb89278aee2370b0
enclar <107608010+enclar@users.noreply.github.com>
2023-03-10 13:22:55 +0800

    feat: edited url path to include api for connection to frontend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('task/', include('task_app.api.urls'))
+    path('api/task/', include('task_app.api.urls'))
 ]
