fd8cf55907395119683a77670bb46abe674234ed
enclar <107608010+enclar@users.noreply.github.com>
2023-03-10 17:46:11 +0800

    feat: updated url and bash.sh file

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/task/', include('task_app.api.urls'))
+    path('api/todo/', include('task_app.api.urls'))
 ]
